8e420c010467b038e8161ad88cf5363f9f806c4f
pisti2000 <nemist186@hengersor.hu>
2025-10-08 14:56:08 +0200

    backend kész + delete,put,post

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 
 
 app.use(cors())
-
+//létrehozzuk az sql lekérdezéshez a kapcsolatot
 const db = mysql.createConnection({
   user:"root",
   host: "127.0.0.1",
@@ -16,24 +16,24 @@ const db = mysql.createConnection({
 });
 
 app.get('/', (req, res) => {
-  res.send('Fut a szerver!');
+  res.send('Fut a szerver!'); 
 });
 
 app.listen(port, () => {
   console.log(`fut a szerver`);
 });
 
-//kiírja a szobák nevét és ágyak számát a szobák táblából
+//kiírja a szobák nevét és ágyak számát minden szobatípusra.
 app.get("/agy", (req,res) =>
   {
       const sql = "select sznev, agy from `szobak`    ";
-      db.query(sql, (err, result) =>{
+      db.query(sql, (err, result) =>{ //lekérdezés végrehajtása
           if(err) return res.json(err); //hiba üzenet visszaadása
-          return res.json(result)
+          return res.json(result)//visszaadja a lekérdezés eredményét 
   
       })
 })
-//kiírja, hogy a szobák mennyire vannak kihasználva és hány vendég hány éjszakát töltött el a szobában
+//kiírja, hogy a szobáknak mennyi a kihasználtsága és hány vendég hány éjszakát töltött el a szobában.
 app.get("/kihasz", (req,res)=>{
   const sql = "SELECT COUNT(foglalasok.vendeg) AS vendégek, SUM(DATEDIFF(foglalasok.tav, foglalasok.erk)) AS vendégéjszakák FROM foglalasok INNER JOIN szobak ON foglalasok.szoba = szobak.szazon GROUP BY szobak.sznev";
   db.query(sql, (err, result) =>{
@@ -50,4 +50,29 @@ app.get("/foglalas", (req,res)=>{
       if(err) return res.json(err);
       return res.json(result)
   })
+})
+//törli a 4-es sorszámú vendéget
+app.delete("/torol", (req,res)=>{
+  const sql = "DELETE FROM vendegek WHERE vsorsz = 4";
+  db.query(sql, (err, result) =>{
+    if(err) return res.json(err);
+    return res.json(result)
+})
+})
+  //a 4-es sorszámú vendéget hozzáadja a "Kiss János" névvel és 6000 irányítószámmal
+app.post("/ujvendeg", (req,res)=>{
+  const sql = "insert into vendegek (vsorsz, vnev, irsz) values (4, 'Kiss János', 6000)";
+  db.query(sql, (err, result) =>{
+    if(err) return res.json(err);
+    return res.json(result)
+})
+})
+
+//módosítja a 3-as sorszámú vendég nevét "Nagy Katalin"-ra
+app.put("/modosit", (req,res)=>{
+  const sql = "UPDATE vendegek SET vnev = 'Nagy Katalin' WHERE vsorsz = 3";
+  db.query(sql, (err, result) =>{
+    if(err) return res.json(err);
+    return res.json(result)
+})
 })
